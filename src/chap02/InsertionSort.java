d4c4ad3e7826e28cf06fa6775509f1567119a17a
hyde1985 <hyde_sun@sina.com>
2018-10-09 13:53:37 +0800

    code for 2-5

---
+++ src/chap02/InsertionSort.java
@@ -0,0 +1,35 @@
+package chap02;
+
+/**
+ * Created by sunchao on 2018-10-9.
+ */
+public class InsertionSort {
+
+    private InsertionSort() {}
+
+    public static void sort(Comparable[] arr) {
+        int n = arr.length;
+
+        for(int i = 1; i < n; i++) {
+            for( int j = i; j > 0; j--) {
+                if(arr[j].compareTo(arr[j - 1]) < 0) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    public static void main(String[] args) {
+
+        int N = 20000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SortTestHelper.testSort("chap02.InsertionSort", arr);
+    }
+}
+
